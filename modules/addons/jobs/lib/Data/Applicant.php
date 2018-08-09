6d26271e688687bbc8653a6e42a3fa3fd4eb22e5
Matthew Watson <matthew.accounts@gmx.com>
2018-08-09 14:13:14 +0100

    Added Applicant model.

---
+++ modules/addons/jobs/lib/Data/Applicant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WHMCS\Module\Addon\Jobs\Data;
+
+// DB abstraction class
+use WHMCS\Database\Capsule;
+
+// ORM class
+use WHMCS\Database\Eloquent\Model;
+
+// https://laravel.com/docs/5.6/eloquent
+class Applicant extends Model {
+	// Table associated with the model
+	protected $table = 'jobs_applicants';
+	protected $primaryKey = 'app_id';
+
+	// We don't have timestamps on this table
+	public $timestamps = false;
+}
+
+?>
