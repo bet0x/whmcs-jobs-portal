61f3a0c5bf6732d0d817d51dbf962dded8e9574b
Matthew Watson <matthew.accounts@gmx.com>
2018-08-09 14:12:11 +0100

    Added Job model

---
+++ modules/addons/jobs/lib/Data/Job.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WHMCS\Module\Addon\Jobs\Data;
+
+// DB abstraction class
+use WHMCS\Database\Capsule;
+
+// ORM class
+use WHMCS\Database\Eloquent\Model;
+
+// https://laravel.com/docs/5.6/eloquent
+class Job extends Model {
+	// Table associated with the model
+	protected $table = 'jobs_joblist';
+	protected $primaryKey = 'job_id';
+
+	// We don't have timestamps on this table
+	public $timestamps = false;
+}
+
+?>
